9a1a1ad81d98fb4b8b0563ca45a72257918ad6d6
blufish1234 <minecraftblufish@163.com>
2024-08-27 13:45:50 +0800

    Edit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix='*', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}!')
+    print(f'Logged in as {bot.user}.')
 
     await bot.change_presence(
         status=discord.Status.idle,
@@ -30,7 +30,10 @@ async def ping(interaction:discord.Interaction):
 
 #隨機數字
 @bot.tree.command(name="隨機數字", description="取得一個隨機數字")
-async def random_number(interaction:discord.Interaction, 起始數字:int = 0, 末尾數字:int = 100):
+async def random_number(interaction:discord.Interaction, 
+    起始數字:int = commands.param(description="抽取的範圍將從這個數字開始（含這個數字），默認爲0",default=0), 
+    末尾數字:int = commands.param(description="抽取的範圍將由這個數字結束（含這個數字），默認爲100", default=100)
+    ):
     number = random.randint(起始數字,末尾數字)
     await interaction.response.send_message(f"隨便想一個數字？\n那就{number}吧！>w<")
 
