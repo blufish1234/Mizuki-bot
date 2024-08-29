b21b3b68b72bf22026f857e54342bbb20220bfd3
blufish1234 <97332474+blufish1234@users.noreply.github.com>
2024-08-29 08:33:16 +0000

    Add more details to /weather command

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import aiohttp
 from http import HTTPStatus
 import weatherapi
 from weatherapi.rest import ApiException
+from datetime import datetime
 
 def token():
     with open("token.txt","r") as file:
@@ -37,7 +38,9 @@ bot.tree.add_command(ping)
 #天氣查詢
 @app_commands.command(name="查詢天氣", description=("使用WeatherAPI.com查詢指定地點的天氣"))
 @app_commands.describe(地區="請輸入地區的英文名稱")
-async def rtweather(interaction:discord.Interaction,地區:str="Taipei"):
+async def rtweather(interaction:discord.Interaction,地區:str):
+    await interaction.response.defer()
+
     apikey = "b11f285dd42f47ac84a21714242808"
 
     configuration = weatherapi.Configuration()
@@ -47,26 +50,50 @@ async def rtweather(interaction:discord.Interaction,地區:str="Taipei"):
     q = 地區 
     lang = 'zh_tw'
 
+    api_response = api_instance.realtime_weather(q,lang=lang)
     try:
-        api_response = api_instance.realtime_weather(q,lang=lang)
+        location = api_response.get("location",{}).get("name")
+        region = api_response.get("location",{}).get("region")
+        country = api_response.get("location",{}).get("country")
         weather_icon = api_response.get("current",{}).get("condition",{}).get("icon")
         weather = api_response.get("current",{}).get("condition",{}).get("text")
         temperature = api_response.get("current",{}).get("temp_c")
-        currenttime = api_response.get("current",{}).get("last_updated")
-
+        lastupdated = api_response.get("current",{}).get("last_updated_epoch")
+        windspeed = api_response.get("current",{}).get("wind_kph")
+        gustspeed = api_response.get("current",{}).get("gust_kph")
+        winddegree = api_response.get("current",{}).get("wind_degree")
+        winddir = api_response.get("current",{}).get("wind_dir")
+        pressure = api_response.get("current",{}).get("pressure_mb")*0.1
+        precipitation = api_response.get("current",{}).get("precip_mm")
+        humidity = api_response.get("current",{}).get("humidity")
+        cloudcover = api_response.get("current",{}).get("cloud")
+        feelslike = api_response.get("current",{}).get("feelslike_c")
+        dewpoint = api_response.get("current",{}).get("dewpoint_c")
+        visibility = api_response.get("current",{}).get("vis_km")
+        uvindex = api_response.get("current",{}).get("uv")
+        
         embed = discord.Embed(
-            title=f"{地區}的實時天氣",
+            title=f"{location}, {region}, {country}的實時天氣",
             color=discord.Color(int("394162",16)),
-            
+            timestamp=datetime.fromtimestamp(lastupdated)
         )
         embed.set_thumbnail(url=f"https:{weather_icon}")
         embed.add_field(name=weather,value="")
         embed.add_field(name="溫度",value=f"{temperature}°C")
-        embed.set_footer(text=f"最後更新時間（本地）：{currenttime}")
-        await interaction.response.send_message(embed=embed)
+        embed.add_field(name="風向&風速",value=f"{windspeed}km/h {winddegree}° {winddir}, 陣風{gustspeed}km/h")
+        embed.add_field(name="大氣壓強",value=f"{round(pressure,2)}KPa")
+        embed.add_field(name="降雨/降雪量",value=f"{precipitation}mm")
+        embed.add_field(name="相對濕度",value=f"{humidity}%")
+        embed.add_field(name="雲層覆蓋度",value=f"{cloudcover}%")
+        embed.add_field(name="體感溫度",value=f"{feelslike}°C")
+        embed.add_field(name="露點溫度",value=f"{dewpoint}°C")
+        embed.add_field(name="能見度",value=f"{visibility}km")
+        embed.add_field(name="紫外線指數",value=f"{uvindex}")
+        embed.set_author(name="WeatherAPI.com")
+        await interaction.followup.send(embed=embed)
 
     except ApiException as e:
-        await interaction.response.send_message("調用API時出錯 Api->realtime_weather: %s\n" % e)
+        await interaction.followup.send("調用API時出錯 Api->realtime_weather: %s\n" % e)
 bot.tree.add_command(rtweather)
 
 #隨機數字
