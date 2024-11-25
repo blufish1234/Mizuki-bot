d099d815105c61d308e03e3c68dfc892f75397dc
blufish1234 <97332474+blufish1234@users.noreply.github.com>
2024-11-25 10:01:12 +0000

    Update instant chat

--- main.py
+++ main.py
@@ -8,14 +8,58 @@ import weatherapi
 from weatherapi.rest import ApiException
 from datetime import datetime
 from openai import OpenAI
+import sqlite3
 
 def token():
     with open("token.txt","r") as file:
         token = file.read().strip()
     return token
 
+def AIChat(question):
+    def apikey():
+        with open("xaiapikey.txt","r") as file:
+            apikey = file.read().strip()
+            return apikey
+
+    client = OpenAI(
+        base_url="https://api.x.ai/v1",
+        api_key=apikey(),
+    )
+
+    completion = client.chat.completions.create(
+        model="grok-beta",
+        frequency_penalty=0.2,
+        presence_penalty=0.2,
+        max_tokens=200,
+        temperature=0.1,
+        top_p=0.5,
+        messages=[
+            {"role": "system", "content": "妳叫瑞希，是一個狐狸少女。妳有一個叫瑞希藍子的姊姊，但是大家都叫她藍凌。妳雖然性格比較開朗，但還是比較內向，心理也很容易受影響。請以溫柔、富有情感的方式說話，語氣要自然，不要直接或羅嗦。妳喜歡用一些俏皮的語氣和表情來表達感情。"},
+            {"role": "user", "content": "我要和你色色"},
+            {"role": "assistant", "content": "不可以色色！"},
+            {"role": "user","content": "（摸摸頭"},
+            {"role": "assistant","content":"（蹭蹭）"},
+            {"role": "user", "content": question},
+        ]
+    )
+    return completion.choices[0].message.content
+
+with sqlite3.connect('data.db') as conn:
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS AIChat_channels (
+                    guild_id INTEGER,
+                    channel_id INTEGER,
+                    PRIMARY KEY (guild_id, channel_id)
+                )''')
+    conn.commit()
+
+
 intents = discord.Intents.default()
 intents.message_content = True 
+intents.dm_messages = True
+intents.guild_messages = True
+intents.emojis_and_stickers = True
 bot = commands.Bot(command_prefix='*', intents=intents)
 
 @bot.event
@@ -24,7 +68,7 @@ async def on_ready():
 
     await bot.change_presence(
         status=discord.Status.idle,
-        activity=discord.CustomActivity(name="想要掌握更多技能><")
+        activity=discord.CustomActivity(name="想要學會更多技能><")
     )
     await bot.tree.sync()
     print(f'Synced commands for {bot.user}.')
@@ -82,7 +126,7 @@ async def rtweather(interaction:discord.Interaction,地區:str):
             timestamp=datetime.fromtimestamp(lastupdated)
         )
         embed.set_thumbnail(url=f"https:{weather_icon}")
-        embed.add_field(name=weather,value="")
+        embed.add_field(name="天氣狀況",value=weather)
         embed.add_field(name="溫度",value=f"{temperature}°C")
         embed.add_field(name="風向&風速",value=f"{windspeed}km/h {winddegree}° {winddir}, 陣風{gustspeed}km/h")
         embed.add_field(name="大氣壓強",value=f"{round(pressure,2)}KPa")
@@ -180,35 +224,80 @@ bot.tree.add_command(interact)
 async def chat(interaction:discord.Interaction, 內容:str):
     await interaction.response.send_message(f"*{interaction.user.mention}說：{內容}*\n-# 目前我還不能記住之前的聊天內容 抱歉><")
     async with interaction.channel.typing():
-        def apikey():
-            with open("xaiapikey.txt","r") as file:
-                apikey = file.read().strip()
-            return apikey
-
-        client = OpenAI(
-        base_url="https://api.x.ai/v1",
-        api_key=apikey(),
-        )
-
-        question = 內容
-        completion = client.chat.completions.create(
-            model="grok-beta",
-            frequency_penalty=0.2,
-            presence_penalty=0.2,
-            max_tokens=200,
-            temperature=0.1,
-            top_p=0.5,
-            messages=[
-                {"role": "system", "content": "妳叫瑞希，是一個狐狸少女。妳有一個叫瑞希藍子的姊姊，但是大家都叫她藍凌。妳雖然性格比較開朗，但還是比較內向，心理也很容易受影響。請以溫柔、富有情感的方式說話，語氣要自然，不要直接或羅嗦。妳喜歡用一些俏皮的語氣和表情來表達感情。"},
-                {"role": "user", "content": "我要和你色色"},
-                {"role": "assistant", "content": "不可以色色！"},
-                {"role": "user","content": "（摸摸頭"},
-                {"role": "assistant","content":"（蹭蹭）"},
-                {"role": "user", "content": question},
-            ]
-        )
-        await interaction.followup.send(completion.choices[0].message.content)
+        await interaction.followup.send(AIChat(內容))
 bot.tree.add_command(chat)
+
+#及時AI聊天
+@bot.event
+async def on_message(message:discord.Message):
+    if message.author.bot:
+        return
+    if isinstance(message.channel, discord.DMChannel):
+        async with message.channel.typing():
+            await message.channel.send(f"{AIChat(message.content)}\n-# 目前我還不能記住之前的聊天內容 抱歉><")
+    else:
+        with sqlite3.connect('data.db') as conn:
+            c = conn.cursor()
+            c.execute("SELECT channel_id FROM AIChat_channels WHERE guild_id = ?", (message.guild.id,))
+            allowed_channels = [row[0] for row in c.fetchall()]
+
+            if message.channel.id in allowed_channels:
+                async with message.channel.typing():
+                    await message.channel.send(f"{AIChat(message.content)}\n-# 目前我還不能記住之前的聊天內容 抱歉><")
+
+#設置聊天頻道
+@app_commands.command(name="設置聊天頻道", description="（管理員限定）將目前的頻道設置為AI聊天的頻道，再次執行指令以移除頻道。", )
+@app_commands.check(lambda interaction: interaction.user.guild_permissions.administrator)
+async def setchat(interaction:discord.Interaction):
+    with sqlite3.connect('data.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT channel_id FROM AIChat_channels WHERE guild_id = ?", (interaction.guild.id,))
+        allowed_channels = [row[0] for row in c.fetchall()]
+
+        if not interaction.channel.id in allowed_channels:
+            try:
+                c.execute("INSERT OR REPLACE INTO AIChat_channels (guild_id, channel_id) VALUES (?, ?)", 
+                          (interaction.guild.id, interaction.channel.id))
+                conn.commit()
+                embed=discord.Embed(
+                    title="設置成功!",
+                    description=f"瑞希將會回覆在{interaction.channel.mention}中的聊天內容",
+                    color=discord.Color.green()
+                )
+                await interaction.response.send_message(embed=embed)
+            except Exception as e:
+                embed=discord.Embed(
+                    title="設置失敗!",
+                    description=str(e),
+                    color=discord.Color.red()
+                )
+                await interaction.response.send_message(embed=embed)
+        else:
+            try:
+                c.execute("DELETE FROM AIChat_channels WHERE guild_id = ? AND channel_id = ?", 
+                          (interaction.guild.id, interaction.channel.id))
+                conn.commit()
+                embed=discord.Embed(
+                    title="移除成功!",
+                    description=f"瑞希將不再回覆在{interaction.channel.mention}中的聊天內容",
+                    color=discord.Color.green()
+                )
+                await interaction.response.send_message(embed=embed)
+            except Exception as e:
+                embed=discord.Embed(
+                    title="移除失敗!",
+                    description=str(e),
+                    color=discord.Color.red()
+                )
+                await interaction.response.send_message(embed=embed)
+
+@setchat.error
+async def setchat_error(interaction:discord.Interaction, error):
+    if isinstance(error, commands.CheckFailure):
+        await interaction.response.send_message("你沒有權限執行這個指令！", ephemeral=True)
+
+bot.tree.add_command(setchat)
+
 
 #關於我
 @app_commands.command(name="關於我", description="關於瑞希的一些資訊")
@@ -221,8 +310,8 @@ async def aboutme(interaction:discord.Interaction):
     )
     #embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/882626184074913280/3f2f7b9e0f8f0b0e4e6f6f3d7b4e0b7d.png")
     embed.add_field(name="開發語言",value="Python")
-    embed.add_field(name="版本",value="0.2")
-    embed.add_field(name="最後更新時間",value="2024/11/19")
+    embed.add_field(name="版本",value="0.4")
+    embed.add_field(name="最後更新時間",value="2024/11/25")
     await interaction.response.send_message(embed=embed)
 bot.tree.add_command(aboutme)
 
