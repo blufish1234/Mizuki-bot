7b14dc9f06a8a750e3ba6bd73add946e5cc6241b
blufish1234 <97332474+blufish1234@users.noreply.github.com>
2025-04-05 13:26:31 +0000

    Update to Chatgpt 4o

--- main.py
+++ main.py
@@ -16,36 +16,36 @@ load_dotenv()
 
 DiscordAPIKey = os.getenv("DISCORDAPI_TOKEN")
 XAIAPIKey = os.getenv("XAI_API_KEY")
-OpenaiAPIKey = os.getenv("OPENAI_API_KEY")
 WeatherAPIKEY = os.getenv("WEATHERAPI_API_KEY")
-if not DiscordAPIKey or not XAIAPIKey or not OpenaiAPIKey or not WeatherAPIKEY:
+if not DiscordAPIKey or not XAIAPIKey or not WeatherAPIKEY:
     raise ValueError("請確保所有環境變數都已設置。")
 
 def AIChat(model,question):
     client = OpenAI(
-        base_url="https://api.x.ai/v1",
-        api_key=XAIAPIKey,
+        #base_url="https://api.x.ai/v1",
+        #api_key=XAIAPIKey,
     )
 
     completion = client.chat.completions.create(
         model=model,
-        frequency_penalty=0.8,
-        presence_penalty=0.2,
+        #frequency_penalty=0.8,
+        #presence_penalty=0.2,
         max_tokens=1024,
         #temperature=0.1,
-        top_p=0.5,
+        #top_p=0.5,
         messages=[
-            {"role": "system", "content": "妳并不是一个助理，而是一位朋友。妳叫瑞希，是一個狐狸少女。妳有一個叫水樹藍子的姊姊，但是大家都叫她藍凌。妳雖然性格開朗，但還是容易憂慮。請以溫柔、感情豐富的方式說話，語氣要自然，也不要說太多。"},
-            {"role": "user", "content": "我要和你色色"},
-            {"role": "assistant", "content": "不可以色色！"},
-            {"role": "user","content": "（摸摸頭"},
-            {"role": "assistant","content":"（蹭蹭）"},
+            #{"role": "system", "content": "妳并不是一个助理，而是一位朋友。妳叫瑞希，是一個狐狸少女。妳有一個叫水樹藍子的姊姊，但是大家都叫她藍凌。妳雖然性格開朗，但還是容易憂慮。請以溫柔、感情豐富的方式說話，語氣要自然，也不要說太多。"},
+            {"role": "system", "content": "妳叫瑞希，是一個狐狸少女。妳有一個姊姊叫水樹藍子，但是大家都叫她藍凌。妳雖然活潑，但是還是會有些憂鬱。說話溫柔富有情感而且簡短，不要問問題，也不要表達個人情感"},
+            #{"role": "user", "content": "我要和你色色"},
+            #{"role": "assistant", "content": "不可以色色！"},
+            #{"role": "user","content": "（摸摸頭"},
+            #{"role": "assistant","content":"（蹭蹭）"},
             {"role": "user", "content": question},
         ]
     )
     return completion.choices[0].message.content
 
-AIModel = "grok-2-latest"
+AIModel = "chatgpt-4o-latest"
 
 def IsAdmin(guild_id, user_role_id):
     with sqlite3.connect('data.db') as conn:
